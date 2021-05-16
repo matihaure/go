ba4c1506125048e20429a44eea34847600f3087e
Matias Haure <matihaure@gmail.com>
2021-05-16 13:10:05 -0300

    Spanish error

--- src/todoapi/controller/crud.go
+++ src/todoapi/controller/crud.go
@@ -3,92 +3,86 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	"github.com/matihaure/model"
 	"github.com/matihaure/views"
-	"net/http"
 )
 
-
-func create(w http.ResponseWriter, r *http.Request){
-		data := views.PostRequest{}
-		json.NewDecoder(r.Body).Decode(&data)
-		fmt.Println(data)
-		if err:= model.CreateTodo(data.Name,data.Todo); err!=nil{
-			w.Write([]byte("Some error"))
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(data)
-}
-
-func readAll(w http.ResponseWriter){
-
-	records,err := model.ReadAll()
-
-	if err!= nil{
+func create(w http.ResponseWriter, r *http.Request) {
+	data := views.PostRequest{}
+	json.NewDecoder(r.Body).Decode(&data)
+	fmt.Println(data)
+	if err := model.CreateTodo(data.Name, data.Todo); err != nil {
 		w.Write([]byte("Some error"))
 		return
 	}
-		json.NewEncoder(w).Encode(records)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(data)
+}
 
+func readAll(w http.ResponseWriter) {
+
+	records, err := model.ReadAll()
+
+	if err != nil {
+		w.Write([]byte("UN ERROR GUACHO!"))
+		return
+	}
+	json.NewEncoder(w).Encode(records)
 
 	w.WriteHeader(http.StatusOK)
 
-
 }
 
-func readbyid(w http.ResponseWriter, data string){
+func readbyid(w http.ResponseWriter, data string) {
 
-	records,err := model.ReadbyId(data)
+	records, err := model.ReadbyId(data)
 
-	if err!= nil{
+	if err != nil {
 		w.Write([]byte("Some error"))
 		return
 	}
 	json.NewEncoder(w).Encode(records)
 
-
 	w.WriteHeader(http.StatusOK)
-
 
 }
 
-func deletebyid(w http.ResponseWriter, data string){
+func deletebyid(w http.ResponseWriter, data string) {
 
-	err:= model.DeleteById(data)
+	err := model.DeleteById(data)
 
-	if err!= nil{
+	if err != nil {
 		w.Write([]byte("Some error"))
 		return
 	}
 
-
 	w.WriteHeader(http.StatusOK)
-
 
 }
 
 func crud() http.HandlerFunc {
 
-	return func(w http.ResponseWriter, r *http.Request){
+	return func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == http.MethodPost{
-		create(w,r)
+		if r.Method == http.MethodPost {
+			create(w, r)
 		}
 
-		if r.Method == http.MethodGet{
+		if r.Method == http.MethodGet {
 
-			data:= r.URL.Query().Get("name")
+			data := r.URL.Query().Get("name")
 
-			if data ==""{
+			if data == "" {
 				readAll(w)
 			}
-			readbyid(w,data)
+			readbyid(w, data)
 		}
 
-		if r.Method == http.MethodDelete{
-			data:= r.URL.Query().Get("name")
-			deletebyid(w,data)
+		if r.Method == http.MethodDelete {
+			data := r.URL.Query().Get("name")
+			deletebyid(w, data)
 		}
 
 	}
